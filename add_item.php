ad4d27baa3e5e4f10bf1736c949d5c6a3baa1cd3
HackRoot1 <98283417+HackRoot1@users.noreply.github.com>
2023-12-30 00:17:54 +0530

    created a add menu page

--- add_item.php
+++ add_item.php
@@ -6,8 +6,15 @@
 
     include("config.php");
 
-    if(isset($_POST['submit'])){
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
 
+        $query = "SELECT * FROM menu WHERE id = '{$id}'";
+        $query_run = mysqli_query($conn, $query); 
+        $data = mysqli_fetch_assoc($query_run);
+    }
+
+    if(isset($_POST['submit'])){
         
         $name = $_POST['food_name'];
         $description = $_POST['description'];
@@ -28,6 +35,33 @@
                         ('{$name}', '{$description}', '{$category}', '{$price}', '{$quantity}', '{$file}' )";
 
         $query_run = mysqli_query($conn, $query) or die("Query failed");
+    }else if(isset($_POST['update'])){
+        $name = $_POST['food_name'];
+        $description = $_POST['description'];
+        $category = $_POST['category'];
+        $price = $_POST['price'];
+        $quantity = $_POST['quantity'];
+
+        // for adding file in folder
+        $file_path = "./assets/images/";
+        $file = $_FILES['food_image']['name'];
+        $temp_file = $_FILES['food_image']['tmp_name'];
+        move_uploaded_file($temp_file, $file_path . $file);
+
+        $query = "  UPDATE  
+                        menu 
+                    SET
+                        name = '{$name}', 
+                        description = '{$description}', 
+                        category = '{$category}',
+                        price = '{$price}' ,
+                        quantity = '{$quantity}' ,
+                        image = '{$file}'
+                    WHERE 
+                        id = {$id}
+                    ";
+
+        $query_run = mysqli_query($conn, $query) or die("Query failed");
     }
 
 ?>
@@ -37,30 +71,41 @@
             <form action="" method = "POST" enctype="multipart/form-data">
                 <div>
                     <label for="">Image</label>
-                    <input type="file" name = "food_image">
+                    <?php 
+                        if(isset($data['image'])){
+                    ?>
+                        <img src = "./assets/images/<?php echo $data['image']; ?>" width="50px" height="50px" />
+                        <input type="file" name = "food_image" value="<?php echo isset($data['image']) ? $data['image'] : "" ?>">
+                    <?php 
+                        }else{
+                    ?>
+                        <input type="file" name = "food_image" value="<?php echo isset($data['image']) ? $data['image'] : "" ?>">
+                    <?php
+                        }
+                    ?>
                 </div>
                 <div>
                     <label for="">Item Name</label>
-                    <input type="text" name = "food_name">
+                    <input type="text" name = "food_name" value="<?php echo isset($data['name']) ? $data['name'] : "" ?>">
                 </div>
                 <div>
                     <label for="">Description</label>
-                    <input type="text" name = "description">
+                    <input type="text" name = "description" value="<?php echo isset($data['description']) ? $data['description'] : "" ?>">
                 </div>
                 <div>
                     <label for="">Category</label>
-                    <input type="text" name = "category">
+                    <input type="text" name = "category" value="<?php echo isset($data['category']) ? $data['category'] : "" ?>">
                 </div>
                 <div>
                     <label for="">Price</label>
-                    <input type="text" name = "price">
+                    <input type="text" name = "price" value="<?php echo isset($data['price']) ? $data['price'] : "" ?>">
                 </div>
                 <div>
                     <label for="">Quantity</label>
-                    <input type="text" name = "quantity">
+                    <input type="text" name = "quantity" value="<?php echo isset($data['quantity']) ? $data['quantity'] : "" ?>">
                 </div>
                 <div>
-                    <input type="submit" name = "submit" value="Submit">
+                    <input type="submit" name = "<?php echo isset($data['quantity']) ? "update"  : "submit" ?>" value="Submit">
                 </div>
             </form>
         </div>
